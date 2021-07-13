214ce25d80d540a2ee3dad7a890445ba0b36ba9f
Phil Hawkins <phil.a.hawkins@gmail.com>
2021-07-13 18:45:16 +1000

    bug

--- build_training.py
+++ build_training.py
@@ -6,8 +6,7 @@ from absl import app, flags, logging
 from pickle import Pickler
 
 from scenario import plan_path
-from environment.truss_env import SpanTruss_GymEnv
-from truss_state import TrussState
+from truss_state import BreakableTrussState
 
 FLAGS = flags.FLAGS
 flags.DEFINE_boolean('debug', False, 'show debug logging messages')
@@ -19,21 +18,18 @@ def main(_argv):
     if FLAGS.debug:
         logging.set_verbosity(logging.DEBUG)
     train_examples = []
-    start_configs = TrussState.get_start_configs(FLAGS.target_dist)
+    start_configs = BreakableTrussState.get_start_configs(FLAGS.target_dist)
     for i, start_config in enumerate(start_configs):
         logging.debug("Running scenario {}".format(i))
-        env = SpanTruss_GymEnv(supports=start_config)
         ex = plan_path(
-            env=env,
-            eps=FLAGS.eps,
+            start_state=BreakableTrussState.from_config(start_config),
             greedy=False,
             heuristic='Manhattan',
+            eps=FLAGS.eps,
             render=False,
-            epsilon=0.,
             return_examples=True
         )
         train_examples.extend(ex)
-        env.close()
 
     logging.info("Saving {} training examples to {}".format(len(train_examples), FLAGS.train_example_path))
     with open(FLAGS.train_example_path, "wb") as f:
