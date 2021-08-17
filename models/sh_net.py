8c2ed38f496cec45b68506c951156d16355ba797
Phil Hawkins <phil.a.hawkins@gmail.com>
2021-08-18 09:45:50 +1000

    removed old baselines

--- models/sh_net.py
+++ models/sh_net.py
@@ -26,23 +26,6 @@ class GATConvW(ConvWrapper):
             heads=args['heads'])
 
 
-class GMMConvW(ConvWrapper):
-    def __init__(self, args):
-        super().__init__(args)
-        self.conv = GMMConv(in_channels=args['node_dims'], out_channels=args['node_dims'], dim=1, kernel_size=args['kernel_size'], separate_gaussians=args['separate_gaussians'])
-
-    def forward(self, x, edge_index, edge_slot, edge_attr, u, batch):
-        # form psuedocoordinates as polar angle fron the center node
-        edge_attr = edge_slot.unsqueeze(dim=-1) * pi / 3
-        return self.conv(x, edge_index, edge_attr=edge_attr)
-
-
-class GCNConvW(ConvWrapper):
-    def __init__(self, args):
-        super().__init__(args)
-        self.conv = GCNConv(in_channels=args['node_dims'], out_channels=args['node_dims'])
-
-
 class GINConvW(ConvWrapper):
     def __init__(self, args):
         super().__init__(args)
@@ -55,38 +38,6 @@ class GINConvW(ConvWrapper):
         self.conv = GINConv(nn=self.node_mlp)
 
 
-class LGC(Module):
-    def __init__(self, args):
-        super().__init__()
-        self._args = args
-        self._cardinality = self._args['node_in_degree']
-        self.node_mlp_1 = MLP(
-            in_features=self._args['node_dims'], 
-            hidden_dims=self._args['node_mlp1_hdims'], 
-            hidden_layers=self._args['node_mlp1_hlayers'], 
-            out_features=self._args['node_dims']
-        )
-        self.node_mlp = MLP(
-            in_features=(self._cardinality + 1) * self._args['node_dims'], 
-            hidden_dims=self._args['node_mlp2_hdims'], 
-            hidden_layers=self._args['node_mlp2_hlayers'], 
-            out_features=self._args['node_dims']
-        )
-
-    def forward(self, x, edge_index, edge_slot, edge_attr, u, batch):
-        N = x.size(0)
-        row, col = edge_index
-        out = self.node_mlp_1(x[row])
-        out = scatter_slots(
-            out_size=(N, self._cardinality, self._args['node_dims']), 
-            node_index=col, 
-            slot_index=edge_slot, 
-            src=out
-        ).view(N, -1)
-        out = torch.cat([out, x], dim=1)
-        out = self.node_mlp(out)
-
-        return out
 
 
 class ResBlock(Module):
@@ -129,12 +80,6 @@ class SHNet(Module):
             out_features=1
         )
         self._attn = GlobalAttention(gate_nn=gate_nn, nn=nn)
-        # self._heuristic_readout = MLP(
-        #     in_features=args['node_dims'],
-        #     hidden_dims=args['heuristic_readout_hdims'],
-        #     hidden_layers=args['heuristic_readout_hlayers'],
-        #     out_features=1
-        # )
         
     def forward(self, g):
         """
@@ -155,8 +100,5 @@ class SHNet(Module):
             )
 
         h = self._attn(x=x, batch=g.node_batch)
-        # h = self._heuristic_readout(x)
-        # # global mean pooling
-        # h = scatter_mean(h, g.node_batch, dim=0)
 
         return h
